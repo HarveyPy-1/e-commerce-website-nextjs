55c5edfcdcb9a2dddc364f45361b60555ce55489
Harvey Ezihe <don4dolex@gmail.com>
2023-11-05 08:03:34 +0000

    Add pagination bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,20 @@
+import PaginationBar from "@/components/PaginationBar";
 import ProductCard from "@/components/ProductCard";
 import { prisma } from "@/lib/db/prisma";
 import Image from "next/image";
 import Link from "next/link";
 
 export default async function Home() {
-  // Code to get all products from the database and order them by 'id' in descending order
+	// Code to get all products from the database and order them by 'id' in descending order
 	const products = await prisma.product.findMany({
 		orderBy: { id: "desc" },
 	});
 
 	return (
-		<div>
-			<p className="text-2xl mb-2">Recently Added </p>
+		<div className="flex flex-col items-center">
+			<div className="flex justify-items-start w-full">
+				<p className="text-2xl mb-2 font-bold">Recently Added </p>
+			</div>
 			<div className="hero rounded-xl bg-base-200">
 				<div className="hero-content flex-col lg:flex-row">
 					<Image
@@ -31,12 +34,15 @@ export default async function Home() {
 					</div>
 				</div>
 			</div>
-			<p className="text-2xl mb-1 mt-2">{"All Products >"}</p>
+			<div className="flex w-full justify-items-start">
+				<p className="text-2xl mb-1 mt-2 font-bold">{"All Products"}</p>
+			</div>
 			<div className="my-4 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
 				{products.slice(1).map((product) => (
 					<ProductCard product={product} key={product.id} />
 				))}
 			</div>
+			<PaginationBar currentPage={3} totalPages={99} />
 		</div>
 	);
 }
